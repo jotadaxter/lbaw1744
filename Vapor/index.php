d32a6cc8beb01bde376ab10e0c9f362afca7eab8
Jotadaxter <jotadaxter@gmail.com>
2018-02-22 10:15:45 +0000

    First Commit - Conteudo temporario

---
+++ Vapor/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jotadaxter
+ * Date: 22/02/2018
+ * Time: 09:56
+ */
+
+//include_once('database/connection.php');
+include_once('templates/common/header.html');
+include_once('templates/common/main_page.html');
+include_once('templates/common/footer.html');
