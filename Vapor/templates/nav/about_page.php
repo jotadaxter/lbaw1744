adfe842f0cce7a390e6b15532c1db8864cab849f
Jotadaxter <jotadaxter@gmail.com>
2018-02-22 10:32:35 +0000

    update

---
+++ Vapor/templates/nav/about_page.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jotadaxter
+ * Date: 22/02/2018
+ * Time: 10:31
+ */
+include_once('templates/common/header.html');
+include_once('templates/nav/about.html');
+include_once('templates/common/footer.html');
