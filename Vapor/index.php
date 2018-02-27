adedc78da7fd5a285df300a48cf1d2b8cd6c2532
Jotadaxter <jotadaxter@gmail.com>
2018-02-27 16:29:40 +0000

    UPDATE BOOTSTRAP

--- Vapor/index.php
+++ Vapor/index.php
@@ -8,5 +8,5 @@
 
 //include_once('database/connection.php');
 include_once('templates/common/header.html');
-include_once('templates/common/main_page.html');
+include_once('templates/common/home_page.html');
 include_once('templates/common/footer.html');
