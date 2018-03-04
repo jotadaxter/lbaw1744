e6cb841fffebf1777cff5ccaaacbe2dd12ef764e
Jotadaxter <jotadaxter@gmail.com>
2018-03-04 00:58:28 +0000

    corrected fix

--- Vapor/templates/common/footer.php
+++ Vapor/templates/common/footer.php
@@ -4,9 +4,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="nav nav-pills nav-justified">
-                    <li><a href="home_page.php">Home</a></li>
-                    <li><a href="about_page.php">About</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a class="line" href="home_page.php">Home</a></li>
+                    <li><a class="line" href="about_page.php">About</a></li>
+                    <li><a class="line" href="#">Contact</a></li>
                 </ul>
             </div>
         </div>
@@ -16,9 +16,9 @@
         <div class="row">
             <div class="col-lg-12" style="margin-bottom: 20px">
                 <ul class="nav nav-pills nav-justified">
-                    <li><a href="/"> &copy; 2018 V a p o r</a></li>
-                    <li><a href="#">Terms of Service</a></li>
-                    <li><a href="#">Privacy</a></li>
+                    <li><a class="line" href="/"> &copy; 2018 V a p o r</a></li>
+                    <li><a class="line" href="#">Terms of Service</a></li>
+                    <li><a class="line" href="#">Privacy</a></li>
                 </ul>
             </div>
         </div>
