54afec227f8242a3fe2975d82e96c0d4e4c81fdd
jotadaxter <jotadaxter@users.noreply.github.com>
2018-03-04 16:26:26 +0000

    update login

--- Vapor/templates/common/login.php
+++ Vapor/templates/common/login.php
@@ -1,14 +1,43 @@
 <div class="container">
     <div class="row">
+        <h3 style="color:white; text-align: center">Login Via<h3>
+        <!-- Facebook Button -->
         <div class="col-lg-4">
-            <a href="#" class="btn btn-lg btn-primary btn-block">Facebook</a>
+            <button id="facebook_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-facebook"></i> Facebook
+            </button>
         </div>
+        <!-- Google Button -->
         <div class="col-lg-4">
-            <a href="#" class="btn btn-lg btn-info btn-block">Google</a>
+            <button id="google_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-google"></i> Google
+            </button>
         </div>
+        <!-- Twitter Button -->
         <div class="col-lg-4">
-            <a href="#" class="btn btn-lg btn-block omb_btn-twitter">Twitter</a>
+            <button id="twitter_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-twitter"></i> Twitter
+            </button>
+        </div>
+        <!-- Steam Button -->
+        <div class="col-lg-4">
+            <button id="steam_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-steam"></i> Steam
+            </button>
+        </div>
+        <!-- LinkedIn Button -->
+        <div class="col-lg-4">
+            <button id="linkedin_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-linkedin"></i> LinkedIn
+            </button>
+        </div>
+        <!-- GitHub Button -->
+        <div class="col-lg-4">
+            <button id="github_button" class="btn btn-lg btn-block generic_login_button">
+                <i class="fa fa-github"></i> GitHub
+            </button>
         </div>
 
     </div>
+    <h3 style="color:white; text-align: center">or<h3>
 </div>
