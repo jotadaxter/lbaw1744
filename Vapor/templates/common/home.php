2bb498ca86fe1a197c3cd391e28e3573491eab4e
Jotadaxter <jotadaxter@gmail.com>
2018-03-04 00:30:39 +0000

    home page

--- Vapor/templates/common/home.php
+++ Vapor/templates/common/home.php
@@ -1,22 +1,24 @@
 <div id="myDIV" class="container">
+    <!-- Slideshow -->
     <section class="jk-slider">
         <div id="carousel-example" class="carousel slide" data-ride="carousel">
+            <!-- Slide Indicator -->
             <ol class="carousel-indicators">
                 <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-example" data-slide-to="1"></li>
                 <li data-target="#carousel-example" data-slide-to="2"></li>
             </ol>
-
-            <div class="carousel-inner">
+            <!-- images -->
+            <div class="carousel-inner slideshow">
                 <div class="item active">
                     <div class="overlay"></div>
-                    <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+                    <a href="#"><img src="res/images/landscape.png" /></a>
                 </div>
                 <div class="item">
-                    <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+                    <a href="#"><img src="res/images/landscape.png" /></a>
                 </div>
                 <div class="item">
-                    <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+                    <a href="#"><img src="res/images/landscape.png" /></a>
                 </div>
             </div>
             <!-- Switch Image buttons -->
@@ -27,7 +29,84 @@
                 <span class="glyphicon glyphicon-chevron-right"></span>
             </a>
         </div>
-
     </section>
+
+    <!-- Image Gallery -->
+    <div class="container gallery" >
+        <div class="row">
+            <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <h1 class="gallery-title"></h1>
+            </div>
+
+            <div align="center">
+                <button class="btn btn-default filter-button" data-filter="all">Popular</button>
+                <button class="btn btn-default filter-button" data-filter="new">Novidades</button>
+                <button class="btn btn-default filter-button" data-filter="promo">Promoções</button>
+            </div>
+            <br/>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter new">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter promo">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter new">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter promo">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter all">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter all">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter promo">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter all">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter all">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter new">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter promo">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter all">
+                <img src="res/images/product.png" class="img-responsive">
+                <figcaption class="figure-caption text-right product-description">Product Description</figcaption>
+            </div>
+        </div>
+    </div>
+    </section>
+
 
 </div>
