2d69b49b9e09160043a98b44c53ebc1020ea22f0
Pedro Azevedo <joao.seta4ever@gmail.com>
2018-03-05 02:52:23 +0000

    Added Single Product Page

---
+++ Vapor/templates/common/product.php
@@ -0,0 +1,114 @@
+<div id="myDIV" class="container">
+<div class="container-fluid">
+	<style>
+	.tag{
+		 box-sizing: content-box;
+		 border-radius: 20px;
+		margin: 0 290px 0 0;
+		padding: 5px 5px 5px 5px;
+		border: 4px solid #000000;
+	}
+</style>
+    <div class="row">
+		<div class="col-md-4">
+            <div class="product-title"><h1>Ableton Live 9.1</div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="res/images/product.png" class="img-rounded" alt="Cinque Terre" width="210" height="210">    
+                    </div>
+                </div>
+				<br>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="res/images/dropbox2.png" class="img-rounded" alt="Cinque Terre" width="70" height="70">    
+                    </div>
+                    <div class="col-md-3">
+                        <img src="res/images/dropbox3.png" class="img-rounded" alt="Cinque Terre" width="70" height="70">    
+                    </div>
+                    <div class="col-md-3">
+                        <img src="res/images/dropbox4.png" class="img-rounded" alt="Cinque Terre" width="70" height="70">    
+                    </div>
+                </div>
+				<br>
+				<div class="row">
+                    <div class="col-md-5">
+                        <button type="button">555€ - 20% = 400€</button>    
+                    </div>
+                    <div class="col-md-4">
+                        <button type="button">Add to Cart!</button>       
+                    </div>
+                </div>
+        
+		</div>
+        <div class="col-md-4">
+			<br><br><br>
+			<br>
+			<br>
+            <div class="title-descriptor"><b>Developer:</b>Ableton</div><br>
+			<div class="product-desc"><b>Publisher:</b>Ableton</div><br>
+			<div class="product-desc"><b>Release Date:</b>24-12-2017</div><br>
+			<div class="product-desc"><b>Rating:</b><img src="res/images/star_full.png" width="20"> <img src="res/images/star_full.png" width="20"></div><br>
+			<div class="product-desc"><b>Systems:</b><img src="res/images/windows_logo.png" width="25"></div><br>
+			  <hr>
+        </div>
+		<div class="col-md-4">
+			<br><br><br>			
+			<div class="product-title"><b>TAGS</b></div><br>
+			<div class="tag">Audio</div>
+			<div class="tag">Aaw</div>
+			<div class="tag">Ableton</div>			
+		</div>
+		<div class="row">
+			<div class="col-md-6"></div>
+			<div class="col-md-6">
+				<div class="title-descriptor"><b>Description:</b></div><br>
+				<div class="title-descriptor">Ableton Live is a software music sequencer and digital audio workstation for macOS and Windows. 
+				The latest major release of Live, Version 10, was released on February 6, 2018. 
+				In contrast to many other software sequencers, Live is designed to be an instrument 
+				for live performances as well as a tool for composing, recording, arranging, mixing, 
+				and mastering, as shown by Ableton's companion hardware product, Ableton Push. It is also 
+				used by DJs, as it offers a suite of controls for beatmatching, crossfading, and other effects 
+				used by turntablists, and was one of the first music applications to automatically beatmatch songs.
+				</div>
+			</div>
+		</div>
+	</div>
+    <hr>
+    <div class="row">
+        <div class="col-md-4">
+    		<ul class="nav nav-tabs nav_tabs">
+				
+				<li class="active"><a href="#service-one" data-toggle="tab">Reviews</a>
+				<br>
+                 <div class="col-md-1"><img src="res/images/profile_picture.png" width="20"></div>
+                 <div class="col-md-4">Janedoe</div>
+                 <div class="col-md-4"><img src="res/images/star_full.png" width="10"><img src="res/images/star_full.png" width="10"><img src="res/images/star_full.png" width="10"></div>
+				</li>
+			</ul>
+			<div class="row">
+				<br>
+				<div class="col-md-12"><p>
+					Vry good<br>
+					The product, I mean. Issa good product. <br>
+					Recommend it vry much to frens and family.<br>
+
+					Kthnx
+				</p>
+				</div>
+			</div>
+        </div>
+		
+		 <div class="col-md-8">
+			<div class="col-md-4">
+				<img src="res/images/product.png" class="img-rounded" alt="Cinque Terre" width="170" height="170">    
+			</div>
+			<div class="col-md-4">
+				<img src="res/images/product.png" class="img-rounded" alt="Cinque Terre" width="170" height="170">    
+			</div>
+			<div class="col-md-4">
+				<img src="res/images/product.png" class="img-rounded" alt="Cinque Terre" width="170" height="170">    
+			</div>
+		 </div>
+    </div>
+</div>
+</div>
