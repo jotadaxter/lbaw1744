a1e2f4d79b90a0fa3f8e0fed255aeafe3eaf7f6c
PORShoterxx <gui-lidio@hotmail.com>
2018-04-10 00:19:16 +0100

    Update query13.sql

--- artefacts/Queries/query13.sql
+++ artefacts/Queries/query13.sql
@@ -1,5 +1,11 @@
 -- Obter Produtos com um minimo de rating
--- SELECT * FROM "Products" WHERE AVG("Products".rating) > $minRating;  
-SELECT * FROM "Products" WHERE "Products". > 1;  
 
---não existe "Products".rating, só se obtem a rating a partir das reviews
+SELECT *
+FROM "Products"
+--WHERE $minRating < (
+WHERE 2. < (
+	SELECT AVG("Reviews".rating) as AvgRating
+	FROM "Reviews"
+	INNER JOIN "SerialKeys" ON "SerialKeys".sk_id = "Reviews".sk_id
+	WHERE "SerialKeys".product_id = "Products".product_id;
+) ORDER BY AvgRating DESC;
