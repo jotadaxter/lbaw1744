5acac6ce9a2bc4fc61ff6745f51170e1b0b58da6
PORShoterxx <gui-lidio@hotmail.com>
2018-04-10 00:04:23 +0100

    Update query12.sql

--- artefacts/Queries/query12.sql
+++ artefacts/Queries/query12.sql
@@ -1,3 +1,7 @@
 -- Obter produtos numa range de valores
---SELECT * FROM "Products" WHERE "Products".price > $minPrice AND "Products".price < $maxPrice;  
-SELECT * FROM "Products" WHERE "Products".price > 20 AND "Products".price < 30;  
+
+SELECT * 
+FROM "Products" 
+LEFT JOIN "Discounts" ON "Products".product_id = "Discounts".product_id
+--WHERE ("Products".price > $minPrice AND "Products".price < $maxPrice) OR ("Discounts".discounted_price > $minPrice AND "Discounts".discounted_price < $maxPrice);
+WHERE ("Products".price > 10. AND "Products".price < 30.) OR ("Discounts".discounted_price > 10. AND "Discounts".discounted_price < 30.);
