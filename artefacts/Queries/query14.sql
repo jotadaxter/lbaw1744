c0b5e1e69bebd911ec65a1553fbd291135098fd8
PORShoterxx <gui-lidio@hotmail.com>
2018-04-10 00:04:01 +0100

    Update query14.sql

--- artefacts/Queries/query14.sql
+++ artefacts/Queries/query14.sql
@@ -1,4 +1,4 @@
 -- Colocar uma querie para buscar os produtos com um certo Sistema Operativo
 
--- SELECT * FROM "Products" WHERE "Products".operating_system = $opSystem;  
-SELECT * FROM "Products" WHERE "Products".operating_system = 'w';
+-- SELECT * FROM "Products" WHERE "Products".operating_system = '%$opSystem%';  
+SELECT * FROM "Products" WHERE "Products".operating_system LIKE '%w%'
