3dcb72dd812fcc49d7f6a1d791dff420e7752973
PORShoterxx <gui-lidio@hotmail.com>
2018-04-10 11:33:49 +0100

    Update queries.sql

--- artefacts/queries.sql
+++ artefacts/queries.sql
@@ -88,3 +88,28 @@ WHERE "Products".product_id IN (
 SELECT product_id 
 FROM "Discounts" 
 WHERE (CURRENT_DATE BETWEEN "Discounts".begin_date AND "Discounts".end_date);
+
+-- Obter produtos numa range de valores
+
+SELECT * 
+FROM "Products" 
+LEFT JOIN "Discounts" ON "Products".product_id = "Discounts".product_id
+WHERE ("Products".price > $minPrice AND "Products".price < $maxPrice) OR ("Discounts".discounted_price > $minPrice AND "Discounts".discounted_price < $maxPrice);
+
+-- Obter Produtos com um minimo de rating
+
+SELECT *
+FROM "Products"
+WHERE $minRating < (
+	SELECT AVG("Reviews".rating) as AvgRating
+	FROM "Reviews"
+	INNER JOIN "SerialKeys" ON "SerialKeys".sk_id = "Reviews".sk_id
+	WHERE "SerialKeys".product_id = "Products".product_id;
+) ORDER BY AvgRating DESC;
+
+-- Colocar uma querie para buscar os produtos com um certo Sistema Operativo
+
+SELECT * FROM "Products" WHERE "Products".operating_system = '%$opSystem%'; 
+
+-- Colocar uma querie que vá buscar as tags de um produto
+SELECT * FROM "Tags" WHERE "Tags".product_id = $product_id;
