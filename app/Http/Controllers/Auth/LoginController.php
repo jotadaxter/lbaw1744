e84cb22510ea0333a8a2d392249a4d93dcbaefb5
Unknown <jotadaxter@gmail.com>
2018-05-02 16:10:49 +0100

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 877c4a909d00304bfa7d71f63f460dd9a7dae689.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Iluminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -37,17 +36,6 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    public function username()
-    {
-        return 'username';
-
-    }
-
-    protected function guard()
-    {
-        return Auth::guard('guard-name');
     }
 
 }
