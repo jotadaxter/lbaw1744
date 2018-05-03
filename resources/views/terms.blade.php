1decf1c3f09b301bce4adfcea0a424e9d3db72d8
nunocr <nunocr964@gmail.com>
2018-05-03 17:36:43 +0100

    Update terms.blade.php

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -7,23 +7,23 @@
             <h2 class="terms_title">Terms and Conditions</h2>
             <ol>
                 <li><strong>Introduction</strong></li>
-            </ol><p>These Website Standard Terms and Conditions written on this webpage shall manage your use of this
-                website. These Terms will be applied fully and affect to your use of this Website.
-                By using this Website, you agreed to accept all terms and conditions written in here. You must not use
+            </ol><p>The Website Standard Terms and Conditions written on this webpage shall manage your use of this
+                website. These Terms will be applied fully and effectively to your use of this Website.
+                By using this Website, you agree to accept all terms and conditions written in here. You must not use
                 this Website if you disagree with any of these Website Standard Terms and Conditions.</p>
             <p>Minors or people below 18 years old are not allowed to use this Website.</p>
             <ol start="2">
                 <li><strong>Intellectual Property Rights</strong></li>
             </ol><p>Other than the content you own, under these Terms, VAPOR and/or its licensors own all the
                 intellectual property rights and materials contained in this Website.</p>
-            <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+            <p>You are granted a limited license only for purposes of viewing the material contained on this Website.</p>
             <ol start="3">
                 <li><strong>Restrictions</strong></li>
             </ol><p>You are specifically restricted from all of the following</p>
             <ul>
-                <li>publishing any Website material in any other media;</li>
-                <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
-                <li>publicly performing and/or showing any Website material;</li>
+                <li>publishing any Website material not belonging to you in any other media;</li>
+                <li>selling, sublicensing and/or otherwise commercializing any Website material not belonging to you;</li>
+                <li>publicly performing and/or showing any Website material not belonging to you;</li>
                 <li>using this Website in any way that is or may be damaging to this Website;</li>
                 <li>using this Website in any way that impacts user access to this Website;</li>
                 <li>using this Website contrary to applicable laws and regulations, or in any way may
@@ -32,8 +32,8 @@
                     other similar activity in relation to this Website;</li>
                 <li>using this Website to engage in any advertising or marketing.</li>
             </ul>
-            <p>Certain areas of this Website are restricted from being access by you and VAPOR may further
-                restrict access by you to any areas of this Website, at any time, in absolute discretion.
+            <p>Certain areas of this Website are restricted from being accessed by you and VAPOR may further
+                restrict access to any areas of this Website, at any time, in absolute discretion.
                 Any user ID and password you may have for this Website are confidential and you must maintain
                 confidentiality as well.</p>
             <ol start="4">
