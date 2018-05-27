0eee294f3df566eb32f5937d41a9186a6c7a87d8
Unknown <jotadaxter@gmail.com>
2018-05-27 13:20:06 +0100

    fix

--- config/app.php
+++ config/app.php
@@ -232,6 +232,7 @@ return [
         'Image' => Intervention\Image\Facades\Image::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        
     ],
 
 ];
