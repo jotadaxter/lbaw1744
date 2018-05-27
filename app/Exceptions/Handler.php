00f22d78f05e1c9b404cab4ee1d507656c9f207f
PORShoterxx <gui-lidio@hotmail.com>
2018-05-27 02:04:04 +0100

    Functional 404 redirect

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace VAPOR\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +49,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+		if($exception instanceof NotFoundHttpException)
+			return response()->view('404', [], 404);
         return parent::render($request, $exception);
     }
 }
