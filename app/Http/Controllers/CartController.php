0b818be9c660b138cff6f3476d5a3c132e30b848
sorryifikillyou <up201306026@fe.up.pt>
2018-05-28 22:57:41 +0100

    Some Stuff

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,9 +11,35 @@ class CartController extends Controller
         return view('cart');
     }
 
-    public function addToCart()
+    public function addToCart($product_id)
     {
         echo 'adding to cart';
+        if(!session()->exists('cart_number')) {
+        	print_r(session('cart'));
+			echo session('cart_number');
+		   $cart_number = 0;
+		   $cart_array[$cart_number] = $product_id;
+		   echo '<br> ola<br>';
+		}	else {
+			print_r(session('cart'));
+			echo session('cart_number');
+			$cart_array = session('cart');
+        	$cart_number = session('cart_number');
+        	
+        	$cart_array[$cart_number] = $product_id;
+        	
+ 		}
+
+ 		
+		// put the array in a session variable
+		$cart_number++;
+		session(['cart' => $cart_array]);
+		session(['cart_number' => $cart_number]);
+
+
+		print_r(session('cart'));
+		echo session('cart_number');
+
         return view('cart');
     }
 }
