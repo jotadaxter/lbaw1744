2fd2a382cdb7a7974369e233db0efb146fed64e7
sorryifikillyou <up201306026@fe.up.pt>
2018-05-30 04:10:20 +0100

    Changes

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -50,18 +50,48 @@ class SearchController extends Controller
             $products = Product::paginate(4);
         }
 
+        $tags = DB::select('SELECT * 
+                            FROM "Tags" ');
 
         return view('products.search')
             ->with(['products' => $products,
-                'old_value' => $search]);
+                'old_value' => $search,
+                'tags' => $tags]);
     }
+
     public function showProductsByTag(Request $request)
     {
         $products = null;
+        if($request->has('search')){
+            $products = Product::search($search)
+                ->paginate(4);
+        }else{
+            $products = Product::paginate(4);
+        }
 
+        $tags = DB::select('SELECT * 
+                            FROM "Tags" ');
+
+        $search = $request->input('search');
+
+        if($request->input('Debian') != null) {
+
+            $products = DB::select('SELECT * 
+            FROM "Products" 
+            WHERE "Products".name LIKE '%$search%' OR EXISTS (
+                SELECT "Tags".tag_name
+                FROM "Tags"
+                WHERE "Products".product_id = "Tags".product_id AND "Tags".tag_name LIKE '%$search%'
+            ) ORDER BY "Products".name ASC;', [$request->input('Debian')]);
+        } 
+
+
+        
 
         return view('products.search')
-            ->with('products', $products);
+            ->with(['products' => $products,
+                    'old_value' => $search,
+                    'tags' => $tags]);
     }
 
 
