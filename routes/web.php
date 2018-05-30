dfa76983c2e222c88162ea0251b0fd5d4124097e
nunocr <nunocr964@gmail.com>
2018-05-30 08:33:32 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -72,8 +72,12 @@ Route::post('profile/{user_id}/products/delete/{product_id}', 'ProfileController
 
 /* Module M03: Reviews and Wish list */
 
+//Show Products in Wishlist
 Route::get('profile/{user_id}/wishlist', 'ProfileController@showWishList')->name('wishlist');
-
+//Add Product To Wishlist
+Route::get('wishlist/add/{product_id}', 'WishlistController@addToWishlist')->name('addProductToWishlist');
+//Remove Product from Wishlist
+Route::post('wishlist/remove/{product_id}', 'WishlistController@removeProduct')->name('removeProduct');
 
 /* Module M04: Cart and Checkout */
 
